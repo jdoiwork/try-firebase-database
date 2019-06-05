ed37bd68f022d95f73976eedad6f6e600100918d
jdoi <jdoi.work@gmail.com>
2019-06-05 18:00:23 +0900

    データベースの読み書き

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("hello javascript")
+// Get a reference to the database service
+var database = firebase.database();
+console.log(database);
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('/users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  });
+}
+
+// read data
+database.ref('/users/618').on('value', function(snapshot){
+  console.log(snapshot.val());
+})
+
+// write data
+writeUserData("618", "jdoi-a", "jdoi.work@gmail.com", "https://jdoi.pw");
+
---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAekebbS8SHGdo0weqkVsT0o42dpLho29U",
+  authDomain: "try-firebase-bc209.firebaseapp.com",
+  databaseURL: "https://try-firebase-bc209.firebaseio.com",
+  projectId: "try-firebase-bc209",
+  //storageBucket: "",
+  messagingSenderId: "259643885585",
+  appId: "1:259643885585:web:596250a06d9bf945"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
