031ebe4ac35fa087f80a3bf3f657714f34b33c57
jdoi <jdoi.work@gmail.com>
2019-06-05 23:04:44 +0900

    更新フォームとデータビュー追加

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 console.log("hello javascript")
 // Get a reference to the database service
 var database = firebase.database();
-console.log(database);
 
 function writeUserData(userId, name, email, imageUrl) {
   firebase.database().ref('/users/' + userId).set({
@@ -11,11 +10,45 @@ function writeUserData(userId, name, email, imageUrl) {
   });
 }
 
-// read data
-database.ref('/users/618').on('value', function(snapshot){
-  console.log(snapshot.val());
+
+
+
+var info = new Vue({
+  el: '#info',
+  data: {
+    info: {},
+  },
+  created: function(){
+    // read data
+    var self = this
+    database.ref('/users/618').on('value', function(snapshot){
+      var val = snapshot.val()
+      console.debug(val)
+      self.updateInfo(val)
+    }, function(error){console.error(error)})
+  },
+
+  methods: {
+    updateInfo: function(val){
+      this.info = val;
+      console.debug("updateInfo", val, this)
+    }
+  }
 })
 
-// write data
-writeUserData("618", "jdoi-a", "jdoi.work@gmail.com", "https://jdoi.pw");
+
+var form = new Vue({
+  el: '#form',
+  data: {
+    name: "jdoi",
+    email: "jdoi.work@gmail.com",
+  },
+  methods: {
+    onSubmit: function(){
+      // write data
+      writeUserData("618", this.name, this.email, "https://jdoi.pw");
+      console.debug("submit")
+    }
+  }
+})
 
