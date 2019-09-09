70502461010a66fb0a0d1a9ffc31d8f432e21cfa
Junichiro Doi <jdoi.work@gmail.com>
2019-09-10 04:49:45 +0900

    database用のviewを専用ファイルに移動

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 console.log("hello javascript")
 
-import * as myservice from './myservice.js'
+// import * as myservice from './myservice.js'
+import * as dbView from './views/database-view.js'
 
 let provider = new firebase.auth.GoogleAuthProvider()
 console.log(provider)
 
-// -----------------------------------
-// info view
-var info = new Vue({
-  el: '#info',
-  data: {
-    info: {},
-  },
-  created: function(){
-    // read data
-    var self = this
-    myservice.subscribe((val) => self.updateInfo(val))
+// // -----------------------------------
+// // info view
+// var info = new Vue({
+//   el: '#info',
+//   data: {
+//     info: {},
+//   },
+//   created: function(){
+//     // read data
+//     var self = this
+//     myservice.subscribe((val) => self.updateInfo(val))
 
-  },
+//   },
 
-  methods: {
-    updateInfo: function(val){
-      this.info = val;
-      console.debug("updateInfo", val, this)
-    }
-  }
-})
+//   methods: {
+//     updateInfo: function(val){
+//       this.info = val;
+//       console.debug("updateInfo", val, this)
+//     }
+//   }
+// })
 
 
-// ----------------------------------------
-// form view
-var form = new Vue({
-  el: '#form',
-  data: myservice.init(),
-  methods: {
-    onSubmit: function(){
-      // write data
-      myservice.post("618", this.name, this.email, "https://jdoi.pw");
-      console.debug("submit")
-    }
-  }
-})
+// // ----------------------------------------
+// // form view
+// var form = new Vue({
+//   el: '#form',
+//   data: myservice.init(),
+//   methods: {
+//     onSubmit: function(){
+//       // write data
+//       myservice.post("618", this.name, this.email, "https://jdoi.pw");
+//       console.debug("submit")
+//     }
+//   }
+// })
 
---
+++ views/database-view.js
@@ -0,0 +1,40 @@
+import * as myservice from '../myservice.js'
+
+// -----------------------------------
+// info view
+var info = new Vue({
+    el: '#info',
+    data: {
+      info: {},
+    },
+    created: function(){
+      // read data
+      var self = this
+      myservice.subscribe((val) => self.updateInfo(val))
+  
+    },
+  
+    methods: {
+      updateInfo: function(val){
+        this.info = val;
+        console.debug("updateInfo", val, this)
+      }
+    }
+  })
+  
+  
+  // ----------------------------------------
+  // form view
+  var form = new Vue({
+    el: '#form',
+    data: myservice.init(),
+    methods: {
+      onSubmit: function(){
+        // write data
+        myservice.post("618", this.name, this.email, "https://jdoi.pw");
+        console.debug("submit")
+      }
+    }
+  })
+  
+export {info, form}
