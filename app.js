0fed314f73f2a5ec4700c62fb96a03947a19b4c2
Junichiro Doi <jdoi.work@gmail.com>
2019-09-10 05:02:27 +0900

    Google認証できるようになるまで

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ import * as dbView from './views/database-view.js'
 let provider = new firebase.auth.GoogleAuthProvider()
 console.log(provider)
 
+function Auth() {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+    console.log("OK", token, user)
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    console.log("error", error)
+  })
+}
+
 var auth = new Vue({
   el: '#auth',
   data: {
@@ -13,6 +34,7 @@ var auth = new Vue({
   methods: {
     signIn: function() {
       console.log("sign in")
+      Auth()
     }
   }
 })
