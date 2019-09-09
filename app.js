97a0b843e72a3b7cd6891985f9adcc3cce612e97
Junichiro Doi <jdoi.work@gmail.com>
2019-09-10 04:56:14 +0900

    sign inボタンをクリックしたらsignInメソッドが呼ばれるようにした

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ import * as dbView from './views/database-view.js'
 
 let provider = new firebase.auth.GoogleAuthProvider()
 console.log(provider)
+
+var auth = new Vue({
+  el: '#auth',
+  data: {
+  },
+
+  methods: {
+    signIn: function() {
+      console.log("sign in")
+    }
+  }
+})
